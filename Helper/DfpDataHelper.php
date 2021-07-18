403f80d59c8cc721246c3b3d6f98ee2af392efc7
James Castro <james.castro@thedrum.com>
2021-07-18 20:26:01 +0100

    Remove doctrine inflector

--- Helper/DfpDataHelper.php
+++ Helper/DfpDataHelper.php
@@ -2,7 +2,6 @@
 
 namespace TheDrum\DfpBundle\Helper;
 
-use Doctrine\Common\Util\Inflector;
 use Symfony\Component\HttpFoundation\RequestStack;
 
 class DfpDataHelper implements DfpDataHelperInterface
@@ -70,7 +69,7 @@ class DfpDataHelper implements DfpDataHelperInterface
                     if ($attributeKey == 'slot_name') {
                         $data = "/{$this->networkId}/{$data}";
                     }
-                    $tmp[Inflector::camelize($attributeKey)] = $data;
+                    $tmp[self::camelize($attributeKey)] = $data;
                 }
 
                 $adUnits[] = $tmp;
@@ -85,6 +84,28 @@ class DfpDataHelper implements DfpDataHelperInterface
     public function setUsedSlots($slots = array())
     {
         $this->usedSlots = $slots;
+    }
+
+    /**
+     * Convert a word in to the format for a Doctrine class name. Converts 'table_name' to 'TableName'
+     *
+     * @param string  $word  Word to classify
+     * @return string $word  Classified word
+     */
+    public static function classify($word)
+    {
+        return str_replace(" ", "", ucwords(strtr($word, "_-", "  ")));
+    }
+
+    /**
+     * Camelize a word. This uses the classify() method and turns the first character to lowercase
+     *
+     * @param string $word
+     * @return string $word
+     */
+    public static function camelize($word)
+    {
+        return lcfirst(self::classify($word));
     }
 
     protected function setRequestTargetingValues()
